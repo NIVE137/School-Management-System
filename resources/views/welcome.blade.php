c35e426e09e81cb438760e8b4fb349b32834b6a6
NIVE137 <nivedj2005@gmail.com>
2026-08-02 07:25:54 +0530

    Fix school logo asset file path and standardize asset helpers across all views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Management System</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('/assets/img/eeschool.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/eeschool.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <style>
